4533cc7439eb2089e0270b6ac2846b0253e4619d
Nicolas Mosquera <nicolas.mosquera@gradiweb.com>
2022-01-09 13:59:31 -0500

    Add config for production mode and modify package.json script

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,27 +1,19 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const { cleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: "/"
+    filename: 'bundle.js'
   },
   resolve: {
     extensions: [
       '.js',
       '.jsx'
-    ], alias: {
-      '@components': path.resolve(__dirname, 'src/components/'),
-      '@styles': path.resolve(__dirname, 'src/styles/')
-    }
+    ]
   },
-  mode: 'production',
   module: {
     rules: [
       {
@@ -54,14 +46,11 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css'
-    }),
-    new cleanWebpackPlugin(),
+    })
   ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new CssMinimizerPlugin(),
-      new TerserPlugin()
-    ]
+  devServer: {
+    watchFiles: ['dist/'],
+    compress: true,
+    port: 3006
   }
 }
