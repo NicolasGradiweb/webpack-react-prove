a93e6543f2d15631fa9256c1d7a5d3eb5201d5cc
Nicolas Mosquera <nicolas.mosquera@gradiweb.com>
2022-01-09 13:02:29 -0500

    Add index.html and import app into index

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/*  Dependencies  */
+import React from 'react';
+import ReactDOM from 'react-dom';
+/*  Components  */
+import App from './components/App';
+
+ReactDOM.render(<App/>, document.getElementById('app'));
