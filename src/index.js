5e9637cb89414e97d55b064a1411b2f763a41a50
Nicolas Mosquera <nicolas.mosquera@gradiweb.com>
2022-01-09 13:45:58 -0500

    Add config for styles and file scss

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 /*  Components  */
 import App from './components/App';
+/*  Styles  */
+import './styles/global.scss'
 
 ReactDOM.render(<App/>, document.getElementById('app'));
